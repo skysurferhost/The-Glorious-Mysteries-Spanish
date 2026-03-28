6256145d794955ad304ea7affbf8253cb18c7c1b
vuongskysurferstorage <vuongskysurferstorage1@gmail.com>
2026-03-28 11:06:46 -0400

    upload

---
+++ data.js
@@ -0,0 +1,274 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-1-c-cha-gisu-sng-li",
+      "name": "1. La Resurrección del Hijo de Dios",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.20950249137904464,
+          "pitch": -0.4250589715753428,
+          "rotation": 0,
+          "target": "1-2-c-cha-gisu-ln-tri"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.16823973861483843,
+          "pitch": -0.6735388679733862,
+          "title": "<p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 115%;\">耶稣复活</span><o:p></o:p></p>",
+          "text": "<p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">安息日既过，一周的第一日，天快亮时，玛利亚玛达肋纳和另外一个玛利亚来看坟墓。忽然发生了大地震，因为上主的天使从天降来，上前把石硕滚开，坐在上面。他的容貌好像闪电，他的衣服洁白如雪。看守的人由于怕他，吓得打颤，变得好像死人一样。天使对妇女说道：「你们不要害怕！我知道你们寻找被钉死的耶稣。他不在这里，因为他已经照他所说的复活了。你们来看看那安放过他的地方；并且快去对他的门徒说：他已经由死者中复活了。看！他在你们以先往加里肋亚去，在那里你们要看见他。看！我已经告诉了你们。」她们赶快离开坟墓，又恐惧又异常喜乐，跑去报告他的门徒。</div></span><o:p></o:p></p><p class=\"MsoNormal\"><br></p><p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">忽然，耶稣迎上她们说：「愿你们平安！」她们遂上前抱住耶稣的脚，朝拜了他。耶稣对她们说：「不要害怕！你们去，报告我的兄弟，叫他们往加里肋亚去，他们要在那里看见我。」<o:p></o:p></span></p><p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">(玛窦福音 28:1–10)</div>"
+        }
+      ]
+    },
+    {
+      "id": "1-2-c-cha-gisu-ln-tri",
+      "name": "二、耶稣升天",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.8295190152248875,
+          "pitch": -0.16886677939099215,
+          "rotation": 0,
+          "target": "2-3-c-cha-thnh-thn-hin-xung"
+        },
+        {
+          "yaw": -2.3393628150142955,
+          "pitch": 0.12362877656975968,
+          "rotation": 0,
+          "target": "0-1-c-cha-gisu-sng-li"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.7809236815983347,
+          "pitch": -0.5434597421898903,
+          "title": "<p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 115%;\">耶稣升天</span><o:p></o:p></p>",
+          "text": "<p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">德敖斐罗！我在第一部书中，已论及耶稣所行所教的一切，直到他藉圣神嘱咐了所选的宗徒之后，被接去的那一天为止；他受难以后，用了许多凭据，向他们显明自己还活着，四十天之久显现给他们，讲论天主国的事。耶稣与他们一起进食时，吩咐他们不要离开耶路撒冷，但要等候父的恩许，即你们听我所说过的：「若翰固然以水施了洗，但不多几天以后，你们要因圣神受洗。」</div></span><o:p></o:p></p><p class=\"MsoNormal\"><br></p><p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">他们聚集的时候，就问耶稣说：「主，是此时要给以色列复兴国家吗？」他回答说：「父以自己的权柄所定的时候和日期，不是你们应当知道的；但当圣神降临于你们身上时，你们将充满圣神的德能，要在耶路撒冷及全犹太和撒玛黎雅，并直到地极，为我作证人。」</div></span><o:p></o:p></p><p class=\"MsoNormal\"><br></p><p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">耶稣说完这些话，就在他们观望中，被举上升，有块云彩接了他去，离开他们的眼界。他们向天注视着他上升的时候，忽有两个穿白衣的人站在他们前，向他们说：「加里肋亚人！你们为什么站着望天呢﹖这位离开你们，被接到天上去的耶稣，你们看见他怎样升了天，也要怎样降来。」<o:p></o:p></span></p><p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">(宗徒大事录 1:1-11)</div>"
+        }
+      ]
+    },
+    {
+      "id": "2-3-c-cha-thnh-thn-hin-xung",
+      "name": "三、圣神降临",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.07506402957144687,
+          "pitch": -0.10977309799296009,
+          "rotation": 0,
+          "target": "3-4-c-cha-tri-cho-c-b-ln-tri"
+        },
+        {
+          "yaw": 1.8835112084244283,
+          "pitch": -0.03659732029855434,
+          "rotation": 0,
+          "target": "1-2-c-cha-gisu-ln-tri"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.11496180811354506,
+          "pitch": -0.37224441421833276,
+          "title": "<p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 115%;\">圣神降临</span><o:p></o:p></p>",
+          "text": "<p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">五旬节日一到，众人都聚集一处。忽然，从天上来了一阵响声，好像暴风刮来，充满了他们所在的全座房屋。有些散开好像火的舌头，停留在他们每人头上，众人都充满了圣神，照圣神赐给他们的话，说起外方话来。</div></span><o:p></o:p></p><p class=\"MsoNormal\"><br></p><p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">那时，居住在耶路撒冷的，有从天下各国来的虔诚的犹太人。这声音一响，就聚来了许多人，都仓皇失措，因为人人都听见他们说自己的方言。他们惊讶奇怪地说：「看，这些说话的不都是加里肋亚人吗？怎么我们每人听见他们说我们出生地的方言呢？我们中有帕提雅人、玛待人、厄蓝人和居住在美索不达米亚、犹太及卡帕多细雅、本都并亚细亚、夫黎基雅和旁非里雅、埃及并靠近基勒乃的利比亚一带的人，以及侨居的罗马人、犹太人和归依犹太教的人、克里特人和阿剌伯人，怎么我们都听见他们用我们的话，讲论天主的奇事呢？」众人都惊讶犹豫，彼此说：「这是什么事？」另有些人却讥笑说：「他们喝醉了酒！」<o:p></o:p></span></p><p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">(宗徒大事录 2:1-13)</div>"
+        }
+      ]
+    },
+    {
+      "id": "3-4-c-cha-tri-cho-c-b-ln-tri",
+      "name": "四、圣母蒙召升天",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.28290339959050925,
+          "pitch": -0.3424279478755068,
+          "rotation": 0,
+          "target": "4-5-c-cha-tri-thng-c-m-trn-tri"
+        },
+        {
+          "yaw": 2.3791351372577685,
+          "pitch": 0.184547861773261,
+          "rotation": 0,
+          "target": "2-3-c-cha-thnh-thn-hin-xung"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.2414294338391887,
+          "pitch": -0.5338737219185532,
+          "title": "<p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 115%;\">圣母蒙召升天</span><o:p></o:p></p>",
+          "text": "<p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">玛利亚遂说：「我的灵魂颂扬上主，我的心神欢跃于天主，我的救主，</div></span><o:p></o:p></p><p class=\"MsoNormal\"><br></p><p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">因为他垂顾了他婢女的卑微，今后万世万代都要称我有福；</div></span><o:p></o:p></p><p class=\"MsoNormal\"><br></p><p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">因全能者在我身上行了大事，他的名字是圣的，</div></span><o:p></o:p></p><p class=\"MsoNormal\"><br></p><p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">他的仁慈世世代代于无穷世，赐与敬畏他的人。</div></span><o:p></o:p></p><p class=\"MsoNormal\"><br></p><p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">他伸出了手臂施展大能，驱散那些心高气傲的人。</div></span><o:p></o:p></p><p class=\"MsoNormal\"><br></p><p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">他从高座上推下权势者，却举扬了卑微贫困的人。</div></span><o:p></o:p></p><p class=\"MsoNormal\"><br></p><p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">他曾使饥饿者饱飨美物，反使那富有者空手而去。</div></span><o:p></o:p></p><p class=\"MsoNormal\"><br></p><p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">他曾回忆起自己的仁慈，扶助了他的仆人以色列，正如他向我们的祖先所说过的恩许，施恩于亚巴郎和他的子孙，直到永远。」<o:p></o:p></span></p><p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">(路加福音 1:46-55)</div>"
+        }
+      ]
+    },
+    {
+      "id": "4-5-c-cha-tri-thng-c-m-trn-tri",
+      "name": "五、圣母为天地之母后",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.749964359811174,
+          "pitch": 0.045768055644552774,
+          "rotation": 0,
+          "target": "3-4-c-cha-tri-cho-c-b-ln-tri"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.6304062715925411,
+          "pitch": -0.20865712269064218,
+          "title": "<p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 115%;\">圣母为天地之母后</span><o:p></o:p></p>",
+          "text": "<p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">那时，天上出现了一个大异兆：有一个女人，身披太阳，脚踏月亮，头戴十二颗星的荣冠；她胎中怀了孕，在产痛和苦劳中，呼疼呻吟。随着天上又出现了另一个异兆：有一条火红的大龙，有七个头，十只角，头上戴着七个王冠。牠的尾巴将天上的星辰勾下了三分之一，投在地上。那条龙便站在那要生产的女人面前，待她生产后，要吞下她的孩子。那女人生了一个男孩子，他就是那要以铁杖牧放万民的；那女人的孩子被提到天主和他的宝座前。女人就逃到旷野去了，在那里有天主已给她准备好的地方，叫她在那里受供养一千二百六十天。</div></span><o:p></o:p></p><p class=\"MsoNormal\"><br></p><p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">以后，天上就发生了战争：弥额尔和他的天使一同与那龙交战，那龙也和牠的使者一起应战，但牠们敌不住，在天上遂再也没有牠们的地方了。于是那大龙被摔了下来，牠就是那远古的蛇，号称魔鬼或撒殚的。那欺骗了全世界的，被摔到地上，牠的使者也同牠一起被摔了下来。那时，我听见天上有大声音说：</div></span><o:p></o:p></p><p class=\"MsoNormal\"><br></p><p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">「如今我们的天主获得了胜利、权能和国度，也显示了他基督的权柄，因为那日夜在我们的天主前，控告我们弟兄的控告者，已被摔下去了。他们赖羔羊的血和他们作证的话，得胜了那条龙，因为他们情愿牺牲自己的性命死了。为此，高天和住在那里的，你们欢乐罢！祸哉，大地和海洋！因为魔鬼怀着大怒下到你们那里去了，因为他知道自己只有很短的时间了。」</div></span><o:p></o:p></p><p class=\"MsoNormal\"><br></p><p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">那条龙一看见自己被摔到地上，就去追赶那生了男婴的女人。可是有大鹰的两个翅膀给了那女人，为叫她飞入旷野，到自己的地方去，在那里受供养一段时期、两段时期和半段时期，远离那蛇的面。那蛇遂在女人后面，从自己的口中吐出一道像河的水，为使那女人被河水冲去；可是大地却援助了那女人，大地裂开一个口，把那条龙从口中所吐出的河水吸了去。那条龙便对那女人大发忿怒，遂去与她其余的后裔，即那些遵行天主的诫命，且为耶稣作证的人交战；于是牠就站在海滩上。<o:p></o:p></span></p><p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">(若望默示录 12:1-18)</div>"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
